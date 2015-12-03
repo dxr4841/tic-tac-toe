ee81bf86b30ecb54469a6a8599c973a546c6bbaa
Darrin Reeves <dxr4841@gmail.com>
2015-12-03 00:52:09 -0500

    Add a mark method

--- lib/board.rb
+++ lib/board.rb
@@ -17,4 +17,14 @@ class Board
       @grid = args[:grid] || (0..8).to_a
       @grid[args[:index]] = args[:marker] if args[:marker] && args[:index]
   end
+
+  def mark(marker, index)
+    if !marker.is_a?(String)
+        raise 'Marker is not a string'
+    elsif marker.length != 1
+        raise 'Marker has to be 1 character'
+    else
+      Board.create({marker: marker, index: index, grid: grid})
+    end
+  end
 end
